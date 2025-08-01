5c6c6dadefe29406c427c92e99f485b5ff714cf2
Andrei <andchir@gmail.com>
2025-08-01 14:27:35 +0300

    Create api2app-chat-widget.js

---
+++ api2app-chat-widget.js
@@ -0,0 +1,182 @@
+/**
+ * Api2AppChatWidget
+ * https://api2app.org
+ */
+class Api2AppChatWidget {
+    constructor(iframeSrc, options = {}) {
+        this.iframeSrc = iframeSrc;
+        this.options = {
+            position: 'bottom-right', // bottom-right, bottom-left, top-right, top-left
+            buttonColor: '#007bff',
+            hoverColor: '#0056b3',
+            ...options
+        };
+
+        this.isOpen = false;
+        this.mediaQuery = window.matchMedia('(max-width: 768px)');
+        this.mediaQueryHandler = (e) => this.handleMediaChange(e); // Сохраняем ссылку на обработчик
+
+        this.init();
+    }
+
+    init() {
+        this.createContainer();
+        this.createIframeBox();
+        this.createButton();
+        this.setupEventListeners();
+        this.handleMediaChange(this.mediaQuery);
+
+        document.body.appendChild(this.container);
+    }
+
+    createContainer() {
+        this.container = document.createElement('div');
+        this.container.style.position = 'fixed';
+        this.container.style.zIndex = '10000';
+        this.container.style.display = 'flex';
+        this.container.style.flexDirection = 'column';
+
+        const positions = {
+            'bottom-right': {bottom: '20px', right: '20px', alignItems: 'flex-end'},
+            'bottom-left': {bottom: '20px', left: '20px', alignItems: 'flex-start'},
+            'top-right': {top: '20px', right: '20px', alignItems: 'flex-end'},
+            'top-left': {top: '20px', left: '20px', alignItems: 'flex-start'}
+        };
+
+        const pos = positions[this.options.position] || positions['bottom-right'];
+        Object.assign(this.container.style, pos);
+    }
+
+    createIframeBox() {
+        this.iframeBox = document.createElement('div');
+        this.iframeBox.style.display = 'none';
+        this.iframeBox.style.marginBottom = '10px';
+        this.iframeBox.style.borderRadius = '10px';
+        this.iframeBox.style.overflow = 'hidden';
+        this.iframeBox.style.boxShadow = '0 4px 12px rgba(0,0,0,0.15)';
+        this.iframeBox.style.backgroundColor = '#fff';
+        this.iframeBox.style.maxWidth = 'calc(100vw - 40px)';
+        this.iframeBox.style.maxHeight = '70vh';
+
+        this.iframe = document.createElement('iframe');
+        this.iframe.src = this.iframeSrc;
+        this.iframe.style.width = '100%';
+        this.iframe.style.height = '100%';
+        this.iframe.style.border = 'none';
+
+        this.iframeBox.appendChild(this.iframe);
+        this.container.appendChild(this.iframeBox);
+    }
+
+    createButton() {
+        this.button = document.createElement('button');
+        this.button.style.width = '60px';
+        this.button.style.height = '60px';
+        this.button.style.borderRadius = '50%';
+        this.button.style.backgroundColor = this.options.buttonColor;
+        this.button.style.border = 'none';
+        this.button.style.cursor = 'pointer';
+        this.button.style.boxShadow = '0 2px 10px rgba(0,0,0,0.2)';
+        this.button.style.display = 'flex';
+        this.button.style.alignItems = 'center';
+        this.button.style.justifyContent = 'center';
+        this.button.style.transition = 'background-color 0.3s, transform 0.1s ease-in-out';
+        this.button.style.flexShrink = '0';
+        this.button.style.transform = 'scale(1)';
+        this.button.style.transformOrigin = 'center center';
+
+        this.button.innerHTML = this.getChatIcon();
+        this.container.appendChild(this.button);
+    }
+
+    setupEventListeners() {
+        this.button.onmouseover = () => {
+            this.button.style.backgroundColor = this.options.hoverColor;
+        };
+        this.button.onmouseout = () => {
+            this.button.style.backgroundColor = this.options.buttonColor;
+        };
+        this.button.onmousedown = () => {
+            this.button.style.transform = 'scale(0.9)';
+        };
+        this.button.onmouseup = () => {
+            this.button.style.transform = 'scale(1)';
+        };
+        this.button.onmouseleave = () => {
+            this.button.style.transform = 'scale(1)';
+        };
+
+        this.button.onclick = (e) => {
+            e.preventDefault();
+            setTimeout(() => {
+                this.toggle();
+            }, 100);
+        };
+
+        this.mediaQuery.addEventListener('change', this.mediaQueryHandler);
+
+        this.resizeHandler = () => {
+            if (this.isOpen) {
+                this.handleMediaChange(this.mediaQuery);
+            }
+        };
+        window.addEventListener('resize', this.resizeHandler);
+    }
+
+    handleMediaChange(e) {
+        if (e.matches) {
+            this.iframeBox.style.width = 'calc(100vw - 40px)';
+            this.iframeBox.style.height = '70vh';
+            this.iframeBox.style.borderRadius = '15px';
+        } else {
+            this.iframeBox.style.width = '350px';
+            this.iframeBox.style.height = '400px';
+            this.iframeBox.style.borderRadius = '10px';
+        }
+    }
+
+    getChatIcon() {
+        return `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0,0,256,256"><g transform="translate(40.96,40.96) scale(0.68,0.68)"><g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(4,4)"><path d="M32,10c13.785,0 25,8.951 25,19.954c0,11.003 -11.215,19.953 -25,19.953c-0.646,0 -1.311,-0.022 -1.983,-0.065c-7.369,5.504 -13.411,6.251 -13.672,6.281c-0.076,0.009 -0.152,0.013 -0.228,0.013c-0.7,0 -1.356,-0.368 -1.72,-0.979c-0.402,-0.678 -0.369,-1.528 0.084,-2.172c0.023,-0.033 1.966,-2.8 4.003,-6.234c-7.138,-3.647 -11.484,-9.924 -11.484,-16.797c0,-11.003 11.215,-19.954 25,-19.954zM32,45.907c11.579,0 21,-7.157 21,-15.954c0,-8.797 -9.421,-15.953 -21,-15.953c-11.579,0 -21,7.157 -21,15.954c0,5.86 4.242,11.243 11.071,14.046c0.529,0.217 0.939,0.65 1.128,1.19c0.189,0.54 0.138,1.135 -0.142,1.634c-0.749,1.343 -1.521,2.635 -2.236,3.787c2.061,-0.888 4.615,-2.27 7.363,-4.401c0.398,-0.31 0.9,-0.458 1.4,-0.412c0.825,0.073 1.638,0.109 2.416,0.109z"></path></g></g></g></svg>`;
+    }
+
+    getArrowIcon() {
+        return `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0,0,256,256"><g transform="translate(40.96,40.96) scale(0.68,0.68)"><g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(4,4)"><path d="M16,14c-0.512,0 -1.02306,0.19494 -1.41406,0.58594c-0.781,0.781 -0.781,2.04712 0,2.82812l14.58594,14.58594l-14.58594,14.58594c-0.781,0.781 -0.781,2.04713 0,2.82812c0.391,0.391 0.90206,0.58594 1.41406,0.58594c0.512,0 1.02306,-0.19494 1.41406,-0.58594l14.58594,-14.58594l14.58594,14.58594c0.781,0.781 2.04713,0.781 2.82812,0c0.781,-0.781 0.781,-2.04713 0,-2.82812l-14.58594,-14.58594l14.58594,-14.58594c0.781,-0.781 0.781,-2.04712 0,-2.82812c-0.781,-0.781 -2.04713,-0.781 -2.82812,0l-14.58594,14.58594l-14.58594,-14.58594c-0.391,-0.391 -0.90206,-0.58594 -1.41406,-0.58594z"></path></g></g></g></svg>`;
+    }
+
+    toggle() {
+        if (this.isOpen) {
+            this.close();
+        } else {
+            this.open();
+        }
+    }
+
+    open() {
+        this.handleMediaChange(this.mediaQuery);
+        this.iframeBox.style.display = 'block';
+        this.button.innerHTML = this.getArrowIcon();
+        this.isOpen = true;
+    }
+
+    close() {
+        this.iframeBox.style.display = 'none';
+        this.button.innerHTML = this.getChatIcon();
+        this.isOpen = false;
+    }
+
+    destroy() {
+        if (this.container && this.container.parentNode) {
+            this.container.parentNode.removeChild(this.container);
+        }
+        if (this.mediaQueryHandler) {
+            this.mediaQuery.removeEventListener('change', this.mediaQueryHandler);
+        }
+        if (this.resizeHandler) {
+            window.removeEventListener('resize', this.resizeHandler);
+        }
+    }
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = ChatWidget;
+}
